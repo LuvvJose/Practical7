7dcc28b7163b6652e0be98dc67799d9626cb8f39
Jose <joseedusolsona@gmail.com>
2025-05-26 21:25:44 +0200

    Add project files.

---
+++ Models/Publication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practical7.Models
+{
+	public abstract class Publication
+	{
+
+        /// <summary>
+        /// Title of the publication.
+        /// </summary>
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public string Summary { get; set; }
+        public string Url { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public abstract string Type { get; }
+        public string GetDate()
+        {
+            return PublicationDate.ToString("dddd, dd MMMM yyyy");
+        }
+    }
+
+    public class Article : Publication
+    {
+        public override string Type => "Article";
+    }
+
+    /// <summary>
+    /// Represents a book publication.
+    /// </summary>
+    public class Book : Publication
+    {
+        public int EditionCount { get; set; }
+        public override string Type => "Book";
+        public string GetEditionDisplay()
+        {
+            return EditionCount == 1 ? "1st Edition" : $"{EditionCount} Editions";
+        }
+
+    }
+}
